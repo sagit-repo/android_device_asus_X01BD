40c4b3b822dc1953cfa17be8f730203cdc52c8da
Danny Baumann <dannybaumann@web.de>
2019-06-22 14:26:09 +0000

    X01BD: Add Touch HAL Hidl for gestures
    Edits:
     - adapt code to work with single node
     - add all supported gestures
     - add custom gesture strings
     - set custom default actions for each gesture
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Ia325ffcdbfd10799b28d00a4d705f680aae00566

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -114,6 +114,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.x01bd
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.x01bd
