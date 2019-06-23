0beacf6801427ba1c36bb19941a435e1f3b5f723
Erfan Abdi <erfangplus@gmail.com>
2019-06-23 14:31:06 +0300

    X01BD: Build sysfs livedisplay
    Change-Id: Ie3d400c4f6950d5d91bd625be0e0d20523f2de0d
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -225,7 +225,8 @@ PRODUCT_PACKAGES += \
 
 # Livedisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Media
 PRODUCT_COPY_FILES += \
