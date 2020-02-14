fbc8b6dbc2fcf9a9600f54cdb7c837914645ce38
Sonal Singh <sonal.singh.19993@gmail.com>
2020-02-14 13:49:14 +0000

    X01BD: Colt-OS Bringup
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- colt_X01BD.mk
+++ colt_X01BD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2020 The MSM-Xtended Project
+# Copyright (C) 2019-2020 The Colt-Enigma Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,14 +19,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Xtended stuff
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common Colt stuff
+$(call inherit-product, vendor/colt/config/common.mk)
 
 # Bootanimation Resolution
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1080x2280
 
-# Official MSM-Xtended
-XTENDED_BUILD_TYPE := OFFICIAL
+# Official Colt-Enigma
+COLT_BUILD_TYPE := OFFICIAL
 
 # Google Connectivity Services
 TARGET_INCLUDE_WIFI_EXT := true
@@ -35,7 +35,7 @@ TARGET_INCLUDE_WIFI_EXT := true
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := xtended_X01BD
+PRODUCT_NAME := colt_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X01BDA
